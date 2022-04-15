4084e63009855ffce609d4dc55c211f90fb918ac
warscraft360 <menut34@gmail.com>
2022-04-15 09:54:48 +0200

    implementing routes system

--- src/open_e/src/App.tsx
+++ src/open_e/src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {  BrowserRouter as Router,  Routes,  Route, Link} from "react-router-dom";
+import Dashboard from './UserSpace/dashboard';
+import Home from './Home/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+      <Router>
+        <Link to="/">Home</Link>
+        <Link to="/Dashboard">Dashbord</Link>
+
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="Dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
