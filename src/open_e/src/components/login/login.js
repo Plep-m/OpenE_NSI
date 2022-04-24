abc901af8afb7602b51e1b1b7c4cda7f1f9c17c3
Paul <menut34@gmail.com>
2022-04-24 16:08:47 +0200

    rollback visual update

--- src/open_e/src/components/login/login.js
+++ src/open_e/src/components/login/login.js
@@ -27,36 +27,23 @@ export default function Login({ setToken }) {
   }
 
   return(
-    <>
     <div className="login-wrapper">
+      <h1>Please Log In</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>Username</p>
+          <input type="text" onChange={e => setUserName(e.target.value)} />
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" onChange={e => setPassword(e.target.value)} />
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </div>
-    <div className="container">
-    <div className="screen">
-      <div className="screen__content">
-        <form className="login">
-          <div className="login__field">
-            <i className="login__icon fas fa-user"></i>
-            <input type="text" className="login__input" placeholder="User name / Email" onChange={e => setUserName(e.target.value)}/>
-          </div>
-          <div className="login__field">
-            <i className="login__icon fas fa-lock"></i>
-            <input type="password" className="login__input" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
-          </div>
-          <button className="button login__submit">
-            <span className="button__text">Log In Now</span>
-            <i className="button__icon fas fa-chevron-right"></i>
-          </button>				
-        </form>
-      </div>
-      <div className="screen__background">
-        <span className="screen__background__shape screen__background__shape4"></span>
-        <span className="screen__background__shape screen__background__shape3"></span>		
-        <span className="screen__background__shape screen__background__shape2"></span>
-        <span className="screen__background__shape screen__background__shape1"></span>
-      </div>		
-    </div>
-  </div>
-  </>
+    
   )
 }
 
