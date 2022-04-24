2bb3cfca069aa00fe0b469ebff1fdad24e7f778b
Paul <menut34@gmail.com>
2022-04-24 16:02:32 +0200

    Fix className

--- src/open_e/src/components/login/login.js
+++ src/open_e/src/components/login/login.js
@@ -28,39 +28,39 @@ export default function Login({ setToken }) {
 
   return(
     <>
-    <div className="login-wrapper">
+    <div classNameName="login-wrapper">
     </div>
-    <div class="container">
-    <div class="screen">
-      <div class="screen__content">
-        <form class="login">
-          <div class="login__field">
-            <i class="login__icon fas fa-user"></i>
-            <input type="text" class="login__input" placeholder="User name / Email" onChange={e => setUserName(e.target.value)}/>
+    <div classNameName="container">
+    <div classNameName="screen">
+      <div className="screen__content">
+        <form className="login">
+          <div className="login__field">
+            <i className="login__icon fas fa-user"></i>
+            <input type="text" className="login__input" placeholder="User name / Email" onChange={e => setUserName(e.target.value)}/>
           </div>
-          <div class="login__field">
-            <i class="login__icon fas fa-lock"></i>
-            <input type="password" class="login__input" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
+          <div className="login__field">
+            <i className="login__icon fas fa-lock"></i>
+            <input type="password" className="login__input" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
           </div>
-          <button class="button login__submit">
-            <span class="button__text">Log In Now</span>
-            <i class="button__icon fas fa-chevron-right"></i>
+          <button className="button login__submit">
+            <span className="button__text">Log In Now</span>
+            <i className="button__icon fas fa-chevron-right"></i>
           </button>				
         </form>
-        <div class="social-login">
+        <div className="social-login">
           <h3>log in via</h3>
-          <div class="social-icons">
-            <a href="#" class="social-login__icon fab fa-instagram"></a>
-            <a href="#" class="social-login__icon fab fa-facebook"></a>
-            <a href="#" class="social-login__icon fab fa-twitter"></a>
+          <div className="social-icons">
+            <a href="#" className="social-login__icon fab fa-instagram"></a>
+            <a href="#" className="social-login__icon fab fa-facebook"></a>
+            <a href="#" className="social-login__icon fab fa-twitter"></a>
           </div>
         </div>
       </div>
-      <div class="screen__background">
-        <span class="screen__background__shape screen__background__shape4"></span>
-        <span class="screen__background__shape screen__background__shape3"></span>		
-        <span class="screen__background__shape screen__background__shape2"></span>
-        <span class="screen__background__shape screen__background__shape1"></span>
+      <div className="screen__background">
+        <span className="screen__background__shape screen__background__shape4"></span>
+        <span className="screen__background__shape screen__background__shape3"></span>		
+        <span className="screen__background__shape screen__background__shape2"></span>
+        <span className="screen__background__shape screen__background__shape1"></span>
       </div>		
     </div>
   </div>
