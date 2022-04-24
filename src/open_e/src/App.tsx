54b3efe13914da8e295a9429d7d866108a0668bf
Paul <menut34@gmail.com>
2022-04-24 13:15:13 +0200

    starting implementing login system

--- src/open_e/src/App.tsx
+++ src/open_e/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import {  BrowserRouter as Router,  Routes,  Route, Link} from "react-router-dom";
 import Dashboard from './UserSpace/dashboard';
+import Login from './components/login/login';
 import Home from './Home/Home';
 
 
 function App() {
+  
+  const [token, setToken] = useState();
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
   return (
       <>
       <Router>
