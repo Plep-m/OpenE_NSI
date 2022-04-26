13980989d308a1177e6c33baed284ddd07076b77
Paul <menut34@gmail.com>
2022-04-26 20:59:12 +0200

    template

--- src/open_e/src/Home/Home.jsx
+++ src/open_e/src/Home/Home.jsx
@@ -56,6 +56,41 @@ export default function Home() {
         </div>
         
       </div>
+      <div class="main-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-3 logo-holder"> 
+          
+          <div class="logo"> <a href="home.html"> <img src="assets/images/logo.png" alt="logo"/> </a> </div>
+          
+           </div>
+        
+        
+        <div class="col-xs-12 col-sm-12 col-md-7 top-search-holder"> 
+          
+          
+          <div class="search-area">
+            <form>
+              <div class="control-group">
+                <ul class="categories-filter animate-dropdown">
+                  <li class="dropdown"> <a class="dropdown-toggle"  data-toggle="dropdown" href="category.html">Categories <b class="caret"></b></a>
+                    <ul class="dropdown-menu" role="menu" >
+                      <li class="menu-header">Computer</li>
+                      <li role="presentation"><a role="menuitem" tabindex="-1" href="category.html">- Clothing</a></li>
+                      <li role="presentation"><a role="menuitem" tabindex="-1" href="category.html">- Electronics</a></li>
+                      <li role="presentation"><a role="menuitem" tabindex="-1" href="category.html">- Shoes</a></li>
+                      <li role="presentation"><a role="menuitem" tabindex="-1" href="category.html">- Watches</a></li>
+                    </ul>
+                  </li>
+                </ul>
+                <input class="search-field" placeholder="Search here..." />
+                <a class="search-button" href="#" ></a> </div>
+            </form>
+          </div>
+          </div>
+          </div>
+          </div>
+          </div>
     </header>
   </body>
 </>
